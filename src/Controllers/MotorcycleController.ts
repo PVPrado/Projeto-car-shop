9a49e6c88453a536b7b97963b4389c4fe1679bbc
Paulo Victor <paulo.v.prado@hotmail.com>
2023-05-03 08:16:40 -0300

    feat: adiciona na camada controller de /motorcycles o getAll e o getByid

--- src/Controllers/MotorcycleController.ts
+++ src/Controllers/MotorcycleController.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import { isValidObjectId } from 'mongoose';
 import MotorcycleService from '../Services/MotorcycleService';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 
@@ -30,6 +31,32 @@ class MotorcyclesController {
     try {
       const newMotorcycle = await this.service.create(motorcycle);
       return this.res.status(201).json(newMotorcycle);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async getAll() {
+    try {
+      const allCars = await this.service.getAll();
+      return this.res.status(200).json(allCars);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async getById() {
+    try {
+      const { id } = this.req.params;
+      if (!isValidObjectId(id)) {
+        return this.res.status(422).json({ message: 'Invalid mongo id' });
+      }
+
+      const carId = await this.service.getById(id);
+      if (!carId) {
+        return this.res.status(404).json({ message: 'Motorcycle not found' });
+      }
+      return this.res.status(200).json(carId);
     } catch (error) {
       this.next(error);
     }
